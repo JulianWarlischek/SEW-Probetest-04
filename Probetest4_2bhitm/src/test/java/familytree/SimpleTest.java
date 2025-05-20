a56a25a3a75c58e8cad8f5eea5cce1709825a8e1
Julian Warlischek <147165428+JulianWarlischek@users.noreply.github.com>
2025-05-20 21:40:17 +0200

    Add files via upload

---
+++ Probetest4_2bhitm/src/test/java/familytree/SimpleTest.java
@@ -0,0 +1,11 @@
+package familytree;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void simpleTest() {
+        System.out.println("Alles funktioniert. Viel Spaß beim Programmieren!");
+    }
+}
